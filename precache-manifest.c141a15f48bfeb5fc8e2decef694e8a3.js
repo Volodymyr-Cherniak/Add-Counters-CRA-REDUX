e415661ef2eb804f2679ea85d445f1279edffd21
Volodymyr-Cherniak <vovastalker26@gmail.com>
2020-03-15 22:29:27 +0100

    Updates

---
+++ precache-manifest.c141a15f48bfeb5fc8e2decef694e8a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c0d099d824739e4bca4cfc16779243",
+    "url": "/Add-Counters-CRA-REDUX/index.html"
+  },
+  {
+    "revision": "cd8d9425d7d68817560a",
+    "url": "/Add-Counters-CRA-REDUX/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "66ef7cf03d418abdb83c",
+    "url": "/Add-Counters-CRA-REDUX/static/css/main.2aa86d63.chunk.css"
+  },
+  {
+    "revision": "cd8d9425d7d68817560a",
+    "url": "/Add-Counters-CRA-REDUX/static/js/2.e8bfee5c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Add-Counters-CRA-REDUX/static/js/2.e8bfee5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ef7cf03d418abdb83c",
+    "url": "/Add-Counters-CRA-REDUX/static/js/main.3fa3000a.chunk.js"
+  },
+  {
+    "revision": "d5fc99b9493bf645f94e",
+    "url": "/Add-Counters-CRA-REDUX/static/js/runtime-main.d2367d2c.js"
+  }
+]);
